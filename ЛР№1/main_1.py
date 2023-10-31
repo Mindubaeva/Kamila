6fbef5e928603868e0c28e9a9cd3230e6d5bb22b
Mindubaeva <148883755+Mindubaeva@users.noreply.github.com>
2023-10-31 20:41:31 +0300

    Update main_1.py
    Исправленная задача

--- ЛР№1/main_1.py
+++ ЛР№1/main_1.py
@@ -1,9 +1,7 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-number_copy = numbers[:]
-numbers[4:5] = []
-numbers_without_none = numbers
-sum_ = sum(numbers_without_none)
-count = len(number_copy)
+numbers[4] = 0
+sum_ = sum(numbers)
+count = len(numbers)
 average = sum_ / count
-number_copy[4:5] = [average]
-print("Измененный список:", number_copy)
+numbers[4:5] = [average]
+print("Измененный список:", numbers)
